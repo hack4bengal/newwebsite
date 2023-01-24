64b8c122780e84f640e942a697cb608b6cfa4283
Tamal <gyansujan69@gmail.com>
2023-01-24 11:56:42 +0530

    fix : Registration marque

--- src/components/infiniteCarousel/InfiniteCarousel.js
+++ src/components/infiniteCarousel/InfiniteCarousel.js
@@ -15,115 +15,255 @@ import sponser10 from '../../assets/images/sponsers/sponser10.png'
 import sponser11 from '../../assets/images/sponsers/sponser11.png'
 
 const InfiniteCarousel = () => {
-  return (
-    <div className="codeutsava__sponsers-carousel-container">
-        <div className="codeutsava__sponsers-carousel">
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser1}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser2}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser3}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser4}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser5}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser6}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser7}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser8}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser9}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser10}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser11}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item-block">
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser1}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser2}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser3}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser4}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser5}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser6}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser7}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser8}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser9}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser10}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser11}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item-block">
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser1}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser2}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser3}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser4}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser5}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser6}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser7}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser8}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser9}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser10}/>
-            </div>
-            <div className="codeutsava__sponsers-carousel-item">
-                <img src={sponser11}/>
-            </div>
+    return (
+        /*  <div className="codeutsava__sponsers-carousel-container">
+             <div className="codeutsava__sponsers-carousel">
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser1} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <h1>HELLO</h1>
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser2} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser3} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser4} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser5} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser6} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser7} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser8} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser9} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser10} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser11} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item-block">
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser1} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser2} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser3} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser4} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser5} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser6} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser7} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser8} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser9} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser10} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser11} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item-block">
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser1} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser2} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser3} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser4} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser5} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser6} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser7} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser8} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser9} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser10} />
+                 </div>
+                 <div className="codeutsava__sponsers-carousel-item">
+                     <img src={sponser11} />
+                 </div>
+             </div>
+         </div> */
+
+        <div className='h4b_reg_text_wrapper'>
+
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+            <p>Registration starting soon</p>
+            <p>Follow our socials !</p>
+
         </div>
-    </div>
-  )
+    )
 }
 
 export default InfiniteCarousel
