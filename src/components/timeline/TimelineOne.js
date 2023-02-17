c4f933748585eec6518158200732e7fb55b84eff
Diganta Kr Banik <digantabanik2000@gmail.com>
2023-02-17 21:47:05 +0530

    improved timeline and timer

--- src/components/timeline/TimelineOne.js
+++ src/components/timeline/TimelineOne.js
@@ -4,41 +4,41 @@ export default function TimelineOne() {
   const [active, setActive] = useState(0);
   const data = [
     {
-      time: "To Be Announced",
+      time: "TBA",
       desc: "Pre-Registrations",
-      date: "To Be Announced",
+      date: "TBA",
       dateDesc: "Pre-Registrations!",
       descInDepth:
         "Join the Excitement, Kick-Start Your Hackathon Experience! The Ideas start brewing in your mind, your imagination becomes wild and you get a thought of something which might help the community!",
     },
     // {
-    //   time: "To Be Announced",
+    //   time: "TBA",
     //   desc: "Pre hackathon session",
-    //   date: "To Be Announced",
+    //   date: "TBA",
     //   dateDesc: "Pre Hackathon Sessions",
     //   descInDepth:
     //     "Pre-hackathon sessions are events or workshops that are held prior to a hackathon.These sessions are designed to provide participants with an introduction to the skills, tools, and technologies that will be used during the hackathon. They may also provide opportunities for participants to network with each other and with mentors, as well as to receive guidance and advice from experts in the field.\n These sessions typically cover a wide range of topics, such as programming languages, design thinking, project management, teamwork, and more. The aim is to help participants become familiar with the tools and technologies that will be used during the hackathon, and to provide a foundation of knowledge for participants to build upon.\n Overall, pre-hackathon sessions are an important part of the hackathon experience and can help participants get the most out of the event.",
     // },
     {
-      time: "To Be Announced",
+      time: "TBA",
       desc: "Hackathon Start - Opening Ceremony",
-      date: "To Be Announced",
+      date: "TBA",
       dateDesc: "Hackathon Start - Opening Ceremony",
       descInDepth:
         "Join us for the kick-off of the Hack4bengal “ - an epic weekend of coding, creativity, and collaboration! we're ready to unleash the power of innovation and creativity. Join us for the opening ceremony, where we'll set the stage for a weekend of coding, problem-solving, and collaboration. On [date]. \n During the opening ceremony, you'll have the opportunity to meet with other participants, hear from industry leaders and experts, and learn about the exciting challenges and opportunities that await you. With prizes, workshops, and mentorship opportunities, this is an event you won't want to miss.\n So, gather your team, sharpen your skills, and get ready to make a difference. We're ready for an unforgettable weekend of innovation, and we hope you are too! Register now and let's get this hackathon started! \n We Welcome everyone to our Hackathon.",
     },
     {
-      time: "To Be Announced",
+      time: "TBA",
       desc: "Hackathon In Progress",
-      date: "To Be Announced",
+      date: "TBA",
       dateDesc: "Hackathon In Progress",
       descInDepth:
         "Get ready to bring your tech skills to the forefront! Hack4Bengal 2.0 registrations will be open. Join us on 'COMING SOON' and get a chance to show your expertise, work on exciting projects, and win amazing prizes. This is a unique opportunity to network with industry experts, learn new technologies, and build something incredible. So, don't miss out! Register now and take the first step towards a fulfilling experience.",
     },
     {
-      time: "To Be Announced",
+      time: "TBA",
       desc: "Hackathon End - Closing Ceremony",
-      date: "To Be Announced",
+      date: "TBA",
       dateDesc: "Hackathon End - Closing Ceremony",
       descInDepth:
         "Winner Time! With things ending, new things will begin. Did we tell about goodies and pizzas? and if that doesn't excite you we dont know what will :P"
