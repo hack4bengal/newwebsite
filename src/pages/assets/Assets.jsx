e6b5e64677351954d28030a84fa1aa404d028010
Tamal <gyansujan69@gmail.com>
2023-03-22 14:47:21 +0530

    fix: assets link updated

--- src/pages/assets/Assets.jsx
+++ src/pages/assets/Assets.jsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import "./Assets.css";
 
 export default function Assets() {
+
+  useEffect(() => {
+    window.location.href = "https://drive.google.com/drive/folders/1rwyYqMLUTWPkpGndF67uXEVczvCJlUMX";
+  }, []);
+
+
   return (
     <div>
       <iframe
