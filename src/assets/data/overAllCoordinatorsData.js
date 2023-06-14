f27aba835cba6e399f849c8f4d436e8e8dcdbedc
AYUSH JHA <72190187+A-jha383@users.noreply.github.com>
2023-06-14 18:24:39 +0530

    Update overAllCoordinatorsData.js

--- src/assets/data/overAllCoordinatorsData.js
+++ src/assets/data/overAllCoordinatorsData.js
@@ -28,7 +28,7 @@ const overAllCoordinaters = [
   {
     Name: "Shubham \n Tribedi",
     Photo: Shubham,
-    position: "Program Coordinator @CodeChef",
+    position: "Chief Comm Catalyst @CodeChef",
     Domain: "Lead Organizer",
     facebook: "https://www.facebook.com/shubham.tribedi",
     twitter: "https://twitter.com/shubham_tribedi",
