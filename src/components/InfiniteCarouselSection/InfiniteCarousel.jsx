591a3377141dc9c19b5fb1ec0edd1e974358e146
Tamal Das <tamalcodes@gmail.com>
2023-06-19 12:16:07 +0530

    feat:changed text for infinite carousel

--- src/components/InfiniteCarouselSection/InfiniteCarousel.jsx
+++ src/components/InfiniteCarouselSection/InfiniteCarousel.jsx
@@ -7,7 +7,7 @@ const InfiniteCarouselSection = () => {
   // create an array of paragraph elements
   const paragraphs = [];
   for (let i = 0; i < numParagraphs; i++) {
-    paragraphs.push(<p key={i}>New Hackathon Dates Announced! Register Now!</p>);
+    paragraphs.push(<p key={i}>Registrations closed. Kindly wait till 20th June for approval/rejection mail.</p>);
   }
 
   return (
