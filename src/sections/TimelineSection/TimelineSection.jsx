69bff43b11c680e77a0fc5cf3ef8404c6a2ed9d2
rohit <rhtry.tech@gmail.com>
2023-07-06 19:53:24 +0530

    update : timeline

--- src/sections/TimelineSection/TimelineSection.jsx
+++ src/sections/TimelineSection/TimelineSection.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import "./TimelineSection.css";
-import { motion } from "framer-motion";
+import {motion} from "framer-motion";
 import Timeline from "../../components/timeline/Timeline.jsx";
 
 const TimelineSection = () => {
   return (
     <>
       <motion.div
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1, transition: { duration: 1 } }}
-        viewport={{ once: false }}
+        initial={{opacity: 0}}
+        whileInView={{opacity: 1, transition: {duration: 1}}}
+        viewport={{once: false}}
         className="hack4bengal__section2"
       >
         <div className="h4b_about_main">
           <div id="timeline">
-            <div >
+            <div>
               <h1>Timeline</h1>
               <hr />
               <Timeline />
