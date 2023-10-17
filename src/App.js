d606655971401ec8e0c66f2ad320ca066570b475
Sagnik Mitra <sagnikmitra123@gmail.com>
2023-10-17 16:39:53 +0530

    Volunteer Route

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import Venue from "./pages/venue/Venue";
 import Brand from "./pages/brand/Brand";
 import Live from "./pages/live/Live";
 import Bingo from "./pages/bingo/Bingo";
+import Volunteers from "./pages/volunteers/volunteers";
 export default class App extends Component {
   render() {
     return (
@@ -41,7 +42,8 @@ export default class App extends Component {
           <Route path="/venue" element={<Venue />} />
           <Route path="/brand" element={<Brand />} />
           <Route path="/live" element={<Live />} />
-          <Route path="/bingo" element={<Bingo />} /> 
+          <Route path="/bingo" element={<Bingo />} />
+          <Route path="/volunteers" element={<Volunteers />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
